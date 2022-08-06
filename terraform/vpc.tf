bd074aedfb1b6a4a9abf113527fdf93ce1c445ba
taki-21 <taki.it.0208.21@gmail.com>
2022-08-07 03:44:08 +0900

    add: vpc, subnetの作成

---
+++ terraform/vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "demo" {
+  cidr_block = "192.168.1.0/24"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "demo_1a" {
+  vpc_id     = aws_vpc.demo.id
+  cidr_block = "192.168.1.0/25"
+  availability_zone = "ap-northeast-1a"
+}
